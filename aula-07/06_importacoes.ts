69fe18fb8127c3fc9154262128b02dfe549ffafe
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-06-08 18:16:04 -0300

    feat: aula 07 - import e export (es modules)

---
+++ aula-07/06_importacoes.ts
@@ -0,0 +1,12 @@
+// named import
+import { pinturas } from "./06_pinturas_named";
+
+// named import com alias
+import { pinturas as ptngs } from "./06_pinturas_named";
+
+// default import
+import paintings from "./06_pinturas_default";
+
+// console.log(paintings)
+// console.log(ptngs)
+console.log(pinturas)
