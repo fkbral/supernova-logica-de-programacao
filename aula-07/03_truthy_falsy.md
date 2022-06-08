24dcf82f544c8a6a026edf1fc6f2cca953d5132e
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-06-08 16:42:02 -0300

    feat: aula 07 - truthy e falsy

---
+++ aula-07/03_truthy_falsy.md
@@ -0,0 +1,14 @@
+# Truthy
+- number
+- string não vazia
+- true
+- array vazio []
+- objeto vazio {}
+- Symbol
+
+# Falsy
+- 0
+- string vazia ""
+- false
+- null
+- undefined
---
+++ aula-07/03_truthy_falsy.ts
@@ -0,0 +1,6 @@
+const valores = [0, "", false, undefined, null, " ", true, [], {}, 10, 0.0000001, -2, () => {}]
+
+valores.forEach(valor => {
+  const resultado = valor ? "truthy" : "falsy"
+  console.log(`${valor} é ${resultado}`)
+})
